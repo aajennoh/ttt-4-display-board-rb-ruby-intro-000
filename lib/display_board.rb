2b0cbe76639057a56f575a83c0bb297e8ced6f5b
JENNIFER OH <ohjennifer.90@gmail.com>
2019-01-30 15:15:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,7 +9,7 @@ def display_board(board)
   puts "   |   |   "
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" X ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts "#{board[0]}  |#{board[1]}  |#{board[2]}   "
   puts "-----------"
