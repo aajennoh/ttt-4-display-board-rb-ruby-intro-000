161048176f4ab42cd28bc76a938fee3264535c9b
JENNIFER OH <ohjennifer.90@gmail.com>
2019-01-30 14:52:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = 9
-board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board
   puts "   |   |   "
